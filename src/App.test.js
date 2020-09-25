d1c6963e7c4ea61235463a8424fb9e785c41b562
sonstein <stein712@gmail.com>
2020-09-26 07:38:08 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/제대로 리액트 배워보자./i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/제대로 리액트 배워보자./i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/제대로 리액트 배워보자./i);
   expect(linkElement).toBeInTheDocument();
 });
